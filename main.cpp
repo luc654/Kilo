6a76d2b89f510d5713ad91f2843c26b07a9aa495
Luc654 <lucas.dieleman@outlook.com>
2025-05-26 20:29:54 +0200

    EOD

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
-
+#include <string>
+#include <cstdint>
 
 using namespace std;
 vector<string> dataset;
 
+struct Token {
+    int chance;
+    string value;
+};
 
-
-
+struct Word {
+    string word;
+    vector<vector<Token>> tokens;
+};
+struct Raw{
+    string word;
+    vector<string> nextPossible;
+};
 void splitString(string& input, char delimiter,
                  string arr[], int& index)
 {
@@ -23,13 +34,13 @@ void splitString(string& input, char delimiter,
 
 
 
-int fillDS(vector<string>& dataset, string data) {
+int fillDS(vector<string>& dataset, string stringData) {
 
     string arrSubset[1000];
     char delimiter = ' ';
     int index = 0;
 
-    splitString(data, delimiter, arrSubset, index);
+    splitString(stringData, delimiter, arrSubset, index);
 
     for(int i = 0; i <  (sizeof(arrSubset) / sizeof(arrSubset[0])); i++){
         if(arrSubset[i].length() > 0){
@@ -43,6 +54,51 @@ int fillDS(vector<string>& dataset, string data) {
 
 
 
+vector<Word> calcProb(vector<string> dataset){
+    for(int i = 0; i < dataset.size(); i++){
+        string prevToken = dataset[i - 1];
+        string token = dataset[i];
+        cout << token << endl;
+    }
+
+    return {{0}};
+}
+
+// 
+// Sort the dataset based on this {firstword, {nextword, nextword, netword}, firstword, {nextword, nexword}}
+// 
+
+vector<vector<string>> filterList(vector<string> dataset){
+    vector<vector<Raw>> list;
+    for(int i = 1; i < dataset.size(); i++){
+        string prevToken = dataset[i - 1];
+        string token = dataset[i];
+        cout << prevToken << " -> " << token << endl;
+
+    }
+
+    return {{"str"}};
+}
+
+int handleWord(string word, string next, vector<vector<Raw>>& list){
+    bool flagg = true;
+    for(int i = 0; i < list.size(); i++){
+        Raw element = list[i];
+        if(element.word == word){
+            flagg = false;
+            element.nextPossible.push_back(next);
+        }
+        cout << element[word];
+    }
+    if(flagg){
+        Raw element = {word, {next}};
+        list.push_back(element);
+    }
+
+    return 0;
+}
+
 int main(){
     fillDS(dataset, "Hello there this is a sentence");   
+    filterList(dataset);
 }
